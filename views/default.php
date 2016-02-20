8c2ab4e67d32657305af559290bdd1db0a94568e
Semenov Alexander <semenov@skeeks.com>
2016-02-20 18:07:15 +0300

    Update default.php

--- views/default.php
+++ views/default.php
@@ -13,9 +13,68 @@
     <?
     $this->registerJs(<<<JS
         VK.init({apiId: {$widget->apiId}, onlyWidgets: true});
-        VK.Widgets.Comments("{$widget->id}", {$widget->getJsonOptions()});
+
+
+        (function(sx, $, _)
+        {
+            sx.classes.VkWidgetComments = sx.classes.Component.extend({
+
+                getResutlOptions: function()
+                {
+                    var options = this.get('options');
+                    var jQWigetWrapper = $("#" + this.get('id'));
+                    var jQWigetWrapperParent = jQWigetWrapper.parent();
+
+                    if (this.get('adaptiveWith'))
+                    {
+                        if (jQWigetWrapperParent.length)
+                        {
+                            options = _.extend(options, {
+                                'width' : jQWigetWrapperParent.width()
+                            });
+                        }
+                    }
+
+                    return options;
+                },
+
+                buildWidget: function()
+                {
+                    VK.Widgets.Comments(this.get('id'), this.getResutlOptions());
+
+                    return this;
+                },
+
+                destroyWidget: function()
+                {
+                    $("#" + this.get('id')).empty();
+                    return this;
+                },
+
+                _onDomReady: function()
+                {
+                    var self = this;
+                    $( window ).resize(function() {
+                       self.destroyWidget().buildWidget();
+                    });
+
+                    this.buildWidget();
+                }
+
+            });
+        })(sx, sx.$, sx._);
+
+        new sx.classes.VkWidgetComments({
+            'adaptiveWith' : {$widget->adaptiveWith},
+            'id' : '{$widget->id}',
+            'options' : {$widget->getJsonOptions()},
+        });
+
+
 JS
     );
     ?>
-    <div id="<?= $widget->id; ?>"></div>
+    <div class="sx-vk-widget-comments-wrapper">
+        <div id="<?= $widget->id; ?>"></div>
+    </div>
 <? endif; ?>
